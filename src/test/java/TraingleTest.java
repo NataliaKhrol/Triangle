9849dff73a158294ffd1a566a7893209aea8d635
NatalyaKhrol <tashapas@yahoo.com>
2022-10-22 12:09:41 +0300

    TriangleFormula was written

--- src/test/java/TraingleTest.java
+++ src/test/java/TraingleTest.java
@@ -1,2 +1,20 @@
-public class TraingleTest {
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TraingleTest implements Triangle{
+
+    @Test
+    void formulaTest() {
+
+        double a = 2;
+        double b = 3;
+        double c = 2;
+        double pExpected = 3.5;
+        double sExpected = 1.98;
+
+        assertEquals(s, sExpected, "Wrong");
+        assertEquals(p, pExpected, "Wrong");
+    }
+
 }
