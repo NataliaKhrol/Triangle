c6b00f6b0734f4d7bffdf55db85f1db72113a587
NatalyaKhrol <tashapas@yahoo.com>
2022-10-22 16:33:31 +0300

    TriangleTest was written

--- src/test/java/TraingleTest.java
+++ src/test/java/TraingleTest.java
@@ -2,19 +2,13 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class TraingleTest implements Triangle{
+public class TraingleTest {
 
     @Test
     void formulaTest() {
 
-        double a = 2;
-        double b = 3;
-        double c = 2;
-        double pExpected = 3.5;
-        double sExpected = 1.98;
-
-        assertEquals(s, sExpected, "Wrong");
-        assertEquals(p, pExpected, "Wrong");
+        double sActual = Triangle.formula(2, 3, 2);
+        double sExpected = 1.984313483298443;
+        assertEquals(sActual, sExpected, "Wrong");
     }
-
 }
