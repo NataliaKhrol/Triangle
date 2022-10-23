68d3f43502e0d71de12e3563a2654302b0eb7ae9
NatalyaKhrol <tashapas@yahoo.com>
2022-10-24 00:03:18 +0300

    Test for negative numbers, and logs are added

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,17 +1,22 @@
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.Random;
 
+@Log4j2
 public class Triangle {
-    public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(formula(random.nextInt(11), random.nextInt(11), random.nextInt(11)));
-    }
 
     @Step("finding the area of a triangle on three sides")
     public static double formula(double a, double b, double c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            log.error("значение сторон не может принимать 0 или меньше 0");
+            throw new RuntimeException("The side of the triangle must be positive");
+        }
+
         double p = (a + b + c) / 2;
+        log.info("находим полупериметр треугольника '{}'", p);
         double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        log.info("находим площадь треугольника '{}'", s);
         return s;
     }
 }
--- src/test/java/TraingleTest.java
+++ src/test/java/TraingleTest.java
@@ -1,4 +1,7 @@
+import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 import static org.testng.Assert.assertEquals;
 
@@ -6,9 +9,35 @@ public class TraingleTest {
 
     @Test
     void formulaTest() {
-
+        final double delta = 0.000_000_001;
         double sActual = Triangle.formula(2, 3, 2);
-        double sExpected = 1.984313483298443;
-        assertEquals(sActual, sExpected, "Wrong");
+        double sExpected = 1.98431348329844;
+        assertEquals(sActual, sExpected, delta, "Wrong");
+    }
+
+    @Test(expectedExceptions = {RuntimeException.class})
+    void negativeNumberTest() {
+
+        Triangle.formula(-2, 3, 2);
     }
 }
+
+
+
+       /* log.fatal("НИЧЕГО НЕ РАБОТАЕТ");
+        log.error("ЧТО-ТО НЕ РАБОТАЕТ");
+        log.warn("ЧТО-ТО МОЖЕТ НЕ РАБОТАТЬ, НО ПОКА ОК");
+        log.info("ИНФА");
+        log.debug("ЭТА ИНФА ПОМОЖЕТ ТЕБЕ НАЙТИ ПРОБЛЕМУ");
+        log.trace("ПОДРОБНАЯ ИНФА ДЛЯ ДЕБАГА");
+
+        @Test
+        void mlA() {
+
+        double a =4.35;
+        double b = 100;
+        final double delta = 0.000_000_001;
+        double c = a*b;
+        assertEquals(435, delta, c);
+    }
+        */
