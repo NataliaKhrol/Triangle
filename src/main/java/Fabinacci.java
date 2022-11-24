1293454307d72b44366645274efc37df0f486473
NatalyaKhrol <tashapas@yahoo.com>
2022-11-24 22:43:24 +0300

    Fibanacci corrected

--- src/main/java/Fabinacci.java
+++ src/main/java/Fabinacci.java
@@ -1,19 +1,19 @@
 import java.util.Arrays;
-// Fibanacci till 1000;
+// Fibanacci till 100;
 
 public class Fabinacci {
     public static void main(String[] args) {
-        int[] fabi = new int[1001];
-        fabi[0] = 0;
+        long[] fabi = new long[101];
+        fabi[0] = 1;
         fabi[1] = 1;
         int count = 2;
-        while (count < 1000) {
-            for (int i = 2; i <= 1000; i++) {
+
+            for (int i = 2; i <= 100; i++) {
                 fabi[i] = fabi[i - 2] + fabi[i - 1];
                 count += 1;
             }
             System.out.println(Arrays.toString(fabi));
         }
     }
-}
+
 
